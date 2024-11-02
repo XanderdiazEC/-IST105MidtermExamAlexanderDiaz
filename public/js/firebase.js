fa521a6bdc785bc81e8a97579845ebffdbf74262
Alexander Diaz <Alexanderjoasdiaz2005@gmail.com>
2024-11-02 15:37:30 -0700

    Connection done

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsGlYZzXPY3XRGH2NgXNkZCNbDIqXx2J8",
+  authDomain: "ist105midtermexamalexand-64f0e.firebaseapp.com",
+  projectId: "ist105midtermexamalexand-64f0e",
+  storageBucket: "ist105midtermexamalexand-64f0e.firebasestorage.app",
+  messagingSenderId: "327920700868",
+  appId: "1:327920700868:web:b0e6964292cf07edebe891"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
